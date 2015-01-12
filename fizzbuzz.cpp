4c7122aba26935c2c7bf896ad14b0e5dcf7d4641
rsterian <rsterian@umich.edu>
2015-01-12 00:46:14 -0500

    Create fizzbuzz.cpp

---
+++ fizzbuzz.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+	for (int x=1; x<=100; x++){
+		if ((x % 15) == 0){
+			cout << "FizzBuzz" << endl;
+		}else if((x % 3) == 0){
+			cout << "Fizz" << endl;
+		}else if((x % 5) == 0){
+			cout << "Buzz" << endl;
+		}else{
+			cout << x << endl;
+		}
+	}
+	return 0;
+}
+  
