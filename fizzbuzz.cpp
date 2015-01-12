27870601cd7bb9f29baeb77d40e5f0ece5fb2d80
Andrew Sterian <a.sterian@emsyde.com>
2015-01-11 21:55:09 -0800

    Major, earth-shaking improvements.

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 int main(){
 	for (int x=1; x<=100; x++){
+
+		cout << x << ": ";
+
 		if ((x % 15) == 0){
 			cout << "FizzBuzz" << endl;
 		}else if((x % 3) == 0){
@@ -11,7 +14,7 @@ int main(){
 		}else if((x % 5) == 0){
 			cout << "Buzz" << endl;
 		}else{
-			cout << x << endl;
+			cout << "no fizz, no buzz" << endl;
 		}
 	}
 	return 0;
